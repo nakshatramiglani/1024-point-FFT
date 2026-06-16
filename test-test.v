eb76983ff979e44b30ca08fa97333d731a90427d
Harivansh Mehta <harivanshmehta4@gmail.com>
2026-06-16 13:14:06 +0530

    Fix add_sub logic

--- test-test.v
+++ test-test.v
@@ -1,10 +1,16 @@
 module bit_reversal_tb;
     reg [2:0] A[7:0];
     wire [2:0] B[7:0];
-    wire [2:0] out[7:0];
+    wire [7:0] out[7:0];
+    reg [7:0] B_new[7:0];
     integer i;
     bit_reversal uut1(.in(A), .out(B));
-    add_sub uut2(.in(B), .out(out));
+    add_sub uut2(.in(B_new), .out(out));
+    always @(*) begin
+        for (i = 0; i < 8; i = i + 1) begin
+            B_new[i] = {5'b0, B[i]};
+        end
+    end
     initial begin
         $dumpfile("test.vcd");
         $dumpvars(0, bit_reversal_tb);
--- test.v
+++ test.v
@@ -1,5 +1,6 @@
 `define WIDTH 8
 `define SIZE 3
+`define DATA_WIDTH 8
 
 /*module bit_reversal(
     input [`SIZE-1:0] in[`WIDTH-1:0],
@@ -35,30 +36,30 @@ module bit_reversal(
 endmodule
 
 module add_sub(
-    input [`SIZE-1:0] in[`WIDTH-1:0],
-    output reg [`SIZE-1:0] out[`WIDTH-1:0]
+    input [`DATA_WIDTH-1:0] in[`WIDTH-1:0],
+    output reg [`DATA_WIDTH-1:0] out[`WIDTH-1:0]
 );
     integer i, j;
-    reg minus;
+    // reg minus;
     reg [`SIZE-1:0] num; 
-    reg [`SIZE-1:0] inter1[`WIDTH-1:0];
-    reg [`SIZE-1:0] inter2[`WIDTH-1:0];
+    reg [`DATA_WIDTH-1:0] inter1[`WIDTH-1:0];
+    reg [`DATA_WIDTH-1:0] inter2[`WIDTH-1:0];
     always @(*) begin
-        minus = 1'b0;
+        // minus = 1'b0;
         for (i = 0; i < `SIZE; i = i + 1) begin
             num = 2**i;
             for (j = 0; j < `WIDTH; j = j + 1) begin
                 if (i == 0) begin
-                    inter1[j] = (minus == 1'b0) ? in[j] + in[j+num] : in[j] - in[j+num];
-                    minus = ~minus;
+                    inter1[j] = ((j & num) == 0) ? in[j] + in[j+num] : in[j-num] - in[j];
+                    // minus = ~minus;
                 end
                 else begin
-                    if (i % (2**i) == 0)
-                        minus = ~minus;
+                    /*if (j % (2**i) == 0)
+                        minus = ~minus;*/
                     if (i % 2 != 0)
-                        inter2[j] = (minus == 1'b0) ? inter1[j] + inter1[j+num] : inter1[j] - inter1[j+num];
+                        inter2[j] = ((j & num) == 0) ? inter1[j] + inter1[j+num] : inter1[j-num] - inter1[j];
                     else
-                        inter1[j] = (minus == 1'b0) ? inter2[j] + inter2[j+num] : inter2[j] - inter2[j+num];
+                        inter1[j] = ((j & num) == 0) ? inter2[j] + inter2[j+num] : inter2[j-num] - inter2[j];
                 end
             end
         end
