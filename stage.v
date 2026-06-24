504010dba2b926ee46467a8f4733a072a4e35a0f
Harivansh Mehta <harivanshmehta4@gmail.com>
2026-06-24 12:35:14 +0530

    Add twiddle instatiation in stage

--- stage.v
+++ stage.v
@@ -11,16 +11,18 @@ module stage #(
 )(
     input logic clk,
     input logic rst_n,
-    input logic signed [DATA_WIDTH-1:0] in_real,
-    input logic signed [DATA_WIDTH-1:0] in_imag,
+    input logic signed [IN_WIDTH-1:0] in_real,
+    input logic signed [IN_WIDTH-1:0] in_imag,
+    input logic [$clog2(WIDTH) - 1:0] sample_count,
 
     output logic signed [FFT_WIDTH-1:0] out_real,
     output logic signed [FFT_WIDTH-1:0] out_imag
 );
 
 localparam SIZE       = $clog2(WIDTH);
-localparam FFT_WIDTH  = DATA_WIDTH + SIZE;
 localparam DATA_WIDTH = IN_WIDTH + STAGE - 1;
+localparam FFT_WIDTH  = DATA_WIDTH + SIZE;
+localparam DELAY = 1 << (SIZE - STAGE);
 
 wire signed [DATA_WIDTH-1:0] delay_in_real;
 wire signed [DATA_WIDTH-1:0] delay_in_imag;
@@ -28,8 +30,10 @@ wire signed [DATA_WIDTH-1:0] delay_in_imag;
 wire signed [DATA_WIDTH-1:0] delayed_real;
 wire signed [DATA_WIDTH-1:0] delayed_imag;
 
-wire signed [15:0] twiddle_real;
-wire signed [15:0] twiddle_imag;
+wire signed [TWIDDLE_WIDTH - 1:0] twiddle_real;
+wire signed [TWIDDLE_WIDTH - 1:0] twiddle_imag;
+
+logic [SIZE - 2:0] angle_idx;
 
 wire signed [DATA_WIDTH-1:0] in_even_real;
 wire signed [DATA_WIDTH-1:0] in_even_imag;
@@ -46,25 +50,40 @@ wire signed [DATA_WIDTH:0] added_imag;
 wire signed [DATA_WIDTH:0] subtracted_real;
 wire signed [DATA_WIDTH:0] subtracted_imag;
 
+assign angle_idx = sample_count << (STAGE - 1);
+
 //generating all buffers
-    buffer #(.DEPTH(1 << (SIZE - STAGE)), .DATA_WIDTH(DATA_WIDTH))
+    buffer #(.DEPTH(DELAY), .DATA_WIDTH(DATA_WIDTH))
         buff_inst(
             .clk(clk),
-            .rst_n(rst_n),
+            .nrst(rst_n),
             .in_real(delay_in_real),
             .in_imag(delay_in_imag),
             .delayed_real(delayed_real),
             .delayed_imag(delayed_imag)
     );
 
+//generate twiddle factors
+    twiddle_factors #(
+        .WIDTH(WIDTH),
+        .TWIDDLE_WIDTH(TWIDDLE_WIDTH)
+    ) twiddle_inst(
+        .clk(clk),
+        .rst_n(rst_n),
+        .angle_idx(angle_idx),
+        .twiddle_real(twiddle_real),
+        .twiddle_imag(twiddle_imag)
+    );
+
 //multiply twiddle factor with even input
     complex_multiply #(.FFT_WIDTH(DATA_WIDTH))
         cmplx_mult_inst(
             .clk(clk),
-            .in1_real(twiddle_real),
-            .in1_imag(twiddle_imag),
-            .in2_real(in_even_real),
-            .in2_imag(in_even_imag),
+            .rst_n(rst_n),
+            .mul1_real(twiddle_real),
+            .mul1_imag(twiddle_imag),
+            .mul2_real(in_even_real),
+            .mul2_imag(in_even_imag),
             .out_real(multiplied_real),
             .out_imag(multiplied_imag)
     );
